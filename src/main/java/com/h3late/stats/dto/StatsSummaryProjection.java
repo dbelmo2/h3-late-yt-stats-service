67c1f1c51efee976387e7044ffcdd68ecd987161
Danny Belmonte <danny.belmonte4@gmail.com>
2026-03-07 14:27:20 -0600

    updated stats summary projection

--- src/main/java/com/h3late/stats/dto/StatsSummaryProjection.java
+++ src/main/java/com/h3late/stats/dto/StatsSummaryProjection.java
@@ -2,10 +2,11 @@ package com.h3late.stats.dto;
 
 public interface StatsSummaryProjection {
     Long getTotal_streams();
+    Double getTotal_late_time_minutes(); //
     Double getAvg_lateness_minutes();
     Integer getRecord_lateness_minutes();
     String getRecord_video_id();
-    String getRecord_title();
+    String getRecord_video_title();
     Long getTotal_late_count();
     Long getTotal_early_count();
     Long getTotal_on_time_count();
