8265de54e40c80895c094a282b048b7287637a31
Danny Belmonte <danny.belmonte4@gmail.com>
2026-03-07 14:48:31 -0600

    temporarily added cors allow all

---
+++ src/main/java/com/h3late/stats/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.h3late.stats.config;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**") // Apply to all endpoints
+                        .allowedOrigins("*") // Allow all domains
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
